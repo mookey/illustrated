9331fa0b91466de2bc9040ec77cc1c417d6ffa09
Staffan Garhammar <staffan.garhammar@viaplay.com>
2015-09-12 14:49:44 +0200

    removed untracked

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('watch', function() {
 
 
 gulp.task('clean', function() {
-  return gulp.src('./dist/**/*.*', { read: false }) // much faster
+  return gulp.src('./public/dist/**/*.*', { read: false }) // much faster
     .pipe(rimraf());
 });
 
